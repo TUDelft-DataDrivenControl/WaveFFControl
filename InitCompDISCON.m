2fe6cdf5d207cf177fcf11d8f6348fc3c608532d
Amr Hegazy <80094244+arhegazy@users.noreply.github.com>
2022-10-25 22:09:35 +0200

    Update InitCompDISCON.m

--- InitCompDISCON.m
+++ InitCompDISCON.m
@@ -9,36 +9,20 @@ TimeStep           =  0.05;        % Simulation time step (s)
 Turbine            = 'NREL5MW';    % Turbines: NREL5MW, DTU10MW, IEA10MW, IEA15MW
 TurbineType        = 'Offshore';   % Turbine foundation type ['Offshore' or 'Onshore']. 
 GenerateDLL        = true;        % A switch for DLL Generation
-% load NREL5MW.mat 
-% 
-% Control.Pitch.Min=-5
-% Str=0.25;
-% Freq_Mixing=0.25*9/126;
-% P_InitAngle=0
-% Kopt=Control.Torque.OptGain;
-% Control.DT=TimeStep;
+
 % FF controller 
 WindSpeed          = 16;           % Operating point used to define the corresponding linear model     
 Kff_gain           = 0.3;          % A static gain to manipulate the intensity of the FF controller  
+
 %% Controller parameters
 % In this section, the turbine parameters are defined 
 DirContents    = cellstr(ls);
-% %TurbParamsFile = DirContents(contains(DirContents,Turbine,'IgnoreCase',true) & endsWith(DirContents, '.m'));      
 TurbParamsFile = fullfile(['ControlParams_', Turbine, '.m']);
 run(TurbParamsFile);
 
 
 %% Controller Compiling
 % This section is responsible for the compilation of the controller
-% However, we should make sure that we delete the previous build files, to
-% ensure that the compiling process will be pulled off independent of the
-% Matlab version being used.
-
-if GenerateDLL == 1    
-    RemoveFolders = DirContents(strcmpi(DirContents, ['slprj', 'rtw']));
-%    rmdir(RemoveFolders, 's')
-    
-    % Now that the folders have been removed the compiling process can take place
-    %rtwbuild('DISCON_NREL5MW_DLL_Generation');
+if GenerateDLL == 1 
     rtwbuild(['DISCON_' Turbine '_WaveFF']);
 end
